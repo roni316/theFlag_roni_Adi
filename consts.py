b07fc240133e9889233ee200aabb4ec3f88e9bad
5talyaqw <talyamalka2000@gmail.com>
2026-09-08 12:57:56 +0300

    Merge branch 'adi' of https://github.com/roni316/theFlag_roni_Adi into adi

--- consts.py
+++ consts.py
@@ -1,3 +1,5 @@
+import pygame.image
+
 BOARD_ROWS = 25
 BOARD_COLS = 50
 CELL_SIZE = 20
@@ -20,3 +22,26 @@ MINE_COLS = 3
 RUNNING_STATE = 1
 LOSE_STATE = 2
 WIN_STATE = 3
+
+BACKGROUND_COLOR = (0, 100, 0)
+EXPLOSION = pygame.image.load('explotion.png')
+FLAG = pygame.image.load('flag.png')
+GRASS = pygame.image.load('grass.png')
+GUARD = pygame.image.load('guard.png')
+INJURY = pygame.image.load('injury.png')
+MINE = pygame.image.load('mine.png')
+SNAKE = pygame.image.load('snake.png')
+SOLIDER = pygame.image.load('soldier.png')
+SOLIDER_NIGHT = pygame.image.load('soldier_night.png')
+TELEPORT = pygame.image.load('teleport.png')
+
+SOLIDER_WIDTH = SOLDIER_COLS * CELL_SIZE
+SOLIDER_HEIGHT = SOLDIER_ROWS * CELL_SIZE
+
+FLAG_WIDTH = FLAG_COLS * CELL_SIZE
+FLAG_HEIGHT = FLAG_ROWS * CELL_SIZE
+FLAG_ROW_DES = BOARD_ROWS - FLAG_ROWS
+FLAG_COL_DES = BOARD_COLS - FLAG_COLS
+
+GRASS_WIDTH = MINE_COLS * CELL_SIZE
+GRASS_HEIGHT = MINE_ROWS * CELL_SIZE
--- screen.py
+++ screen.py
@@ -1,22 +1,37 @@
-BOARD_ROWS = 25
-BOARD_COLS = 50
-CELL_SIZE = 20
-WINDOW_WIDTH = BOARD_COLS * CELL_SIZE
-WINDOW_HEIGHT = BOARD_ROWS * CELL_SIZE
-
-#This page consts
-BACKGROUND_COLOR = (138, 201, 38)
-
 import pygame
+import consts
+import random
+
+def create_regular_screen():
+    window.fill(consts.BACKGROUND_COLOR)
+    solider = pygame.transform.scale(consts.SOLIDER, (consts.SOLIDER_WIDTH, consts.SOLIDER_HEIGHT))
+    window.blit(solider,(0,0))
+    flag = pygame.transform.scale(consts.FLAG, (consts.FLAG_WIDTH, consts.FLAG_HEIGHT))
+    window.blit(flag,( consts.FLAG_COL_DES * consts.CELL_SIZE, consts.FLAG_ROW_DES * consts.CELL_SIZE))
+    grass = pygame.transform.scale(consts.GRASS, (consts.GRASS_WIDTH, consts.GRASS_HEIGHT))
+    destination = get_random_location()
+    for i in range(consts.MINES_COUNT):
+        window.blit(grass,(destination[i][1] * consts.CELL_SIZE, destination[i][0] * consts.CELL_SIZE))
+
+def get_random_location():
+    location_list = []
+    row = random.sample(range(1,consts.BOARD_ROWS-1), consts.MINES_COUNT)
+    col = random.sample(range(1,consts.BOARD_COLS-3), consts.MINES_COUNT)
+    for i in range(len(row)):
+        add = (row[i], col[i])
+        location_list.append(add)
+    return location_list
 pygame.init()
 
-window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+window = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
 pygame.display.set_caption("Adi and Roni")
-window.fill(BACKGROUND_COLOR)
 run = True
+create_regular_screen()
 pygame.display.flip()
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 pygame.quit()
+
+
