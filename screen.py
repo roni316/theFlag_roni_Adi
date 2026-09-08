44728e87ce0ace07a39f6ddb4b12ee848a019cd4
5talyaqw <talyamalka2000@gmail.com>
2026-09-08 15:06:44 +0300

    Merge branch 'adi' of https://github.com/roni316/theFlag_roni_Adi into adi

--- screen.py
+++ screen.py
@@ -1,6 +1,7 @@
 import pygame
 import consts
 import random
+import game_field
 
 def create_regular_screen():
     window.fill(consts.BACKGROUND_COLOR)
@@ -23,10 +24,29 @@ def get_random_location():
     return location_list
 pygame.init()
 
+def create_night_screen():
+    create_grid()
+    for i in range(len(game_field.board)):
+        for j in range(len(game_field.board[i])):
+            if game_field.board[i][j] == "MINE":
+                mine = pygame.transform.scale(consts.MINE, (consts.GRASS_WIDTH, consts.GRASS_HEIGHT))
+                window.blit(mine, (j * consts.CELL_SIZE, i * consts.CELL_SIZE))
+            if game_field.board[i][j] == "SOLIDER":
+                solider = pygame.transform.scale(consts.SOLIDER, (consts.SOLIDER_WIDTH, consts.SOLIDER_HEIGHT))
+                window.blit(solider, (j * consts.CELL_SIZE, i * consts.CELL_SIZE))
+
+def create_grid():
+    blockSize = 20
+    for x in range(0, consts.WINDOW_WIDTH, blockSize):
+        for y in range(0, consts.WINDOW_HEIGHT, blockSize):
+            rect = pygame.Rect(x, y, blockSize, blockSize)
+            pygame.draw.rect(window, "dark green", rect, 1)
+
 window = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
 pygame.display.set_caption("Adi and Roni")
 run = True
-create_regular_screen()
+create_night_screen()
+# create_regular_screen()
 pygame.display.flip()
 while run:
     for event in pygame.event.get():
