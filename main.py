1f3093f95f56cae1d356bb6e15d0ad7057403219
git <talulafer@gmail.com>
2026-09-09 16:12:52 +0300

    Merge branch 'roni' of https://github.com/roni316/theFlag_roni_Adi into adi new

--- main.py
+++ main.py
@@ -11,32 +11,32 @@ state = {
 
 
 
-def main():
-    while state["is_window_open"]:
-        handle_user_events()
+# def main():
+#     while state["is_window_open"]:
+#         handle_user_events()
 
 
 
-def handle_user_events():
-    for event in pygame.event.get():
-
-        if event.type == pygame.QUIT:
-            state["is_window_open"] = False
-
-        elif state["state"] != consts.RUNNING_STATE:
-            continue
-
-        if event.key == pygame.K_LEFT:
-            soldier.move_soldier_left()
-
-        if event.key == pygame.K_RIGHT:
-            soldier.move_soldier_right()
-
-        if event.key == pygame.K_UP:
-            soldier.move_soldier_up()
-
-        # if event.key == pygame.K_DOWN:
-        #     soldier.move_soldier_down()
+# def handle_user_events():
+#     for event in pygame.event.get():
+#
+#         if event.type == pygame.QUIT:
+#             state["is_window_open"] = False
+#
+#         elif state["state"] != consts.RUNNING_STATE:
+#             continue
+#
+#         if event.key == pygame.K_LEFT:
+#             soldier.move_soldier_left()
+#
+#         if event.key == pygame.K_RIGHT:
+#             soldier.move_soldier_right()
+#
+#         if event.key == pygame.K_UP:
+#             soldier.move_soldier_up()
+#
+#         # if event.key == pygame.K_DOWN:
+#         #     soldier.move_soldier_down()
 
 
 
