68ee66cd8e647e329d43ae705a88a0feead4f018
git <talulafer@gmail.com>
2026-09-09 09:24:50 +0300

    Merge branch 'roni' of https://github.com/roni316/theFlag_roni_Adi into adi new

--- soldier.py
+++ soldier.py
@@ -1,29 +1,65 @@
-import consts
 import game_field
 
 from consts import *
 
+
+#moves the soldier left in the matrix
 def move_soldier_left():
     for row in range(len(game_field.final_board())):
         for col in range(len(game_field.final_board()[row])):
-            if game_field.final_board()[row][col] == SOLIDER_BOX:
+            if game_field.final_board()[row][col] == SOLIDER_BODY_BOX:
                 game_field.final_board()[row][col] = EMPTY_BOX
-                game_field.final_board()[row][col+1] = SOLIDER_BOX
+                game_field.final_board()[row][col+1] = SOLIDER_BODY_BOX
+
+    for row in range(len(game_field.final_board())):
+        for col in range(len(game_field.final_board()[row])):
+            if game_field.final_board()[row][col] == SOLIDER_FEET_BOX:
+                game_field.final_board()[row][col] = EMPTY_BOX
+                game_field.final_board()[row][col+1] = SOLIDER_FEET_BOX
 
 
-
-
+#moves the soldier right in the matrix
 def move_soldier_right():
+    for row in range(len(game_field.final_board())):
+        for col in range(len(game_field.final_board()[row])):
+            if game_field.final_board()[row][col] == SOLIDER_FEET_BOX:
+                game_field.final_board()[row][col] = EMPTY_BOX
+                game_field.final_board()[row][col+1] = SOLIDER_FEET_BOX
+
     for row in range(len(game_field.final_board()),0):
         for col in range(len(game_field.final_board()[row]),0):
-            if game_field.final_board()[row][col] == SOLIDER_BOX:
+            if game_field.final_board()[row][col] == SOLIDER_BODY_BOX:
                 game_field.final_board()[row][col] = EMPTY_BOX
-                game_field.final_board()[row][col - 1] = SOLIDER_BOX
+                game_field.final_board()[row][col - 1] = SOLIDER_BODY_BOX
 
+
+#moves the soldier up in the matrix
 def move_soldier_up():
-    pass
+    for row in range(len(game_field.final_board())):
+        for col in range(len(game_field.final_board()[row])):
+            if game_field.final_board()[row][col] == SOLIDER_BODY_BOX:
+                game_field.final_board()[row][col] = EMPTY_BOX
+                game_field.final_board()[row - 1][col] = SOLIDER_BODY_BOX
 
+    for row in range(len(game_field.final_board())):
+        for col in range(len(game_field.final_board()[row])):
+            if game_field.final_board()[row][col] == SOLIDER_FEET_BOX:
+                game_field.final_board()[row][col] = EMPTY_BOX
+                game_field.final_board()[row - 1][col] = SOLIDER_FEET_BOX
+
+
+#moves the soldier down in the matrix
 def move_soldier_down():
-    pass
+    for row in range(len(game_field.final_board())):
+        for col in range(len(game_field.final_board()[row])):
+            if game_field.final_board()[row][col] == SOLIDER_FEET_BOX:
+                game_field.final_board()[row][col] = EMPTY_BOX
+                game_field.final_board()[row + 1][col] = SOLIDER_FEET_BOX
+
+    for row in range(len(game_field.final_board()), 0):
+        for col in range(len(game_field.final_board()[row]), 0):
+            if game_field.final_board()[row][col] == SOLIDER_BODY_BOX:
+                game_field.final_board()[row][col] = EMPTY_BOX
+                game_field.final_board()[row + 1][col] = SOLIDER_BODY_BOX
 
 
